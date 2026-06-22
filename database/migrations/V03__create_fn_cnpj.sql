d40418ba04125f2731a09623bede5dfae7019c4c
msinis <eduarda.sinis@gmail.com>
2026-06-22 17:31:23 -0300

    feat: add CNPJ validation function

--- database/migrations/V03__create_fn_cnpj.sql
+++ database/migrations/V03__create_fn_cnpj.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION exercicio4.fn_cnpj(IN p_cnpj TEXT)
+CREATE OR REPLACE FUNCTION functions.cnpj(IN p_cnpj TEXT)
 RETURNS BOOLEAN
 LANGUAGE plpgsql
 AS
@@ -11,81 +11,54 @@ DECLARE
     v_num2 INTEGER;
     i INTEGER;
 BEGIN
-    -- Remove qualquer caractere não numérico
     v_cnpj := regexp_replace(p_cnpj, '\D', '', 'g');
 
-    -- Deve ter exatamente 14 dígitos
     IF length(v_cnpj) <> 14 THEN
         RETURN FALSE;
     END IF;
 
-    -- Rejeita sequências repetidas
-    IF v_cnpj IN (
-        '00000000000000',
-        '11111111111111',
-        '22222222222222',
-        '33333333333333',
-        '44444444444444',
-        '55555555555555',
-        '66666666666666',
-        '77777777777777',
-        '88888888888888',
-        '99999999999999'
-    ) THEN
+    -- rejeita sequência repetida
+    IF v_cnpj ~ '^(.)\1{13}$' THEN
         RETURN FALSE;
     END IF;
 
-    -- PRIMEIRO DÍGITO 
+    -- primeiro dígito
     v_sum := 0;
     v_weight := 5;
 
     FOR i IN 1..12 LOOP
         v_sum := v_sum +
-            CAST(substr(v_cnpj, i, 1) AS INTEGER) * v_weight;
+            CAST(SUBSTRING(v_cnpj FROM i FOR 1) AS INTEGER) * v_weight;
 
         v_weight := v_weight - 1;
-
         IF v_weight < 2 THEN
             v_weight := 9;
         END IF;
     END LOOP;
 
     v_num1 := v_sum % 11;
+    v_num1 := CASE WHEN v_num1 < 2 THEN 0 ELSE 11 - v_num1 END;
 
-    IF v_num1 < 2 THEN
-        v_num1 := 0;
-    ELSE
-        v_num1 := 11 - v_num1;
-    END IF;
-
-    -- SEGUNDO DÍGITO
-     
+    -- segundo dígito
     v_sum := 0;
     v_weight := 6;
 
     FOR i IN 1..13 LOOP
         v_sum := v_sum +
-            CAST(substr(v_cnpj, i, 1) AS INTEGER) * v_weight;
+            CAST(SUBSTRING(v_cnpj FROM i FOR 1) AS INTEGER) * v_weight;
 
         v_weight := v_weight - 1;
-
         IF v_weight < 2 THEN
             v_weight := 9;
         END IF;
     END LOOP;
 
     v_num2 := v_sum % 11;
+    v_num2 := CASE WHEN v_num2 < 2 THEN 0 ELSE 11 - v_num2 END;
 
-    IF v_num2 < 2 THEN
-        v_num2 := 0;
-    ELSE
-        v_num2 := 11 - v_num2;
-    END IF;
-
-    -- Valida os dígitos finais
     RETURN
-        v_num1 = CAST(substr(v_cnpj, 13, 1) AS INTEGER)
+        v_num1 = CAST(SUBSTRING(v_cnpj FROM 13 FOR 1) AS INTEGER)
         AND
-        v_num2 = CAST(substr(v_cnpj, 14, 1) AS INTEGER);
+        v_num2 = CAST(SUBSTRING(v_cnpj FROM 14 FOR 1) AS INTEGER);
 END;
 $$;
