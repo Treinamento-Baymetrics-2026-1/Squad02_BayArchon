d77bd9406c1e84b36a00287358a3135001c13dd4
Guilherme Francini <86390834+GuiFrancini@users.noreply.github.com>
2026-06-22 15:35:31 -0300

    feat: Initial structure of the register page

--- front-end/src/routes/router.tsx
+++ front-end/src/routes/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Login } from "@/pages/login";
+//import { Login } from "@/pages/login";
+import { Register } from "@/pages/registerUse";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <Register />,
   },
   /* 
   {
