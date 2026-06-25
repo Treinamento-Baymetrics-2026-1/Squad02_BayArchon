248da3e5c241ecee0dfd752c33c855002b2a0dc2
msinis <eduarda.sinis@gmail.com>
2026-06-25 11:43:34 -0300

    style: types of logs

--- database/migrations/V18__create_enum_logs_types.sql
+++ database/migrations/V18__create_enum_logs_types.sql
@@ -8,28 +8,35 @@ BEGIN
           AND n.nspname = 'logs'
     ) THEN
         CREATE TYPE logs.type_logs AS ENUM (
-            'status_changed_t_documents',
+            'document_status_changed',
             'document_created',
             'document_updated',
+
             'contract_created',
             'contract_expired',
             'contract_updated',
             'contract_type_created',
             'contract_type_updated',
+
             'profile_created',
             'profile_status_changed',
             'profile_updated',
+
             'sector_created',
             'sector_updated',
+
             'category_created',
             'category_updated',
-            'project_status_changed',
+
+            'project_status_changed_created', -- colocar cada tipo de status
             'project_created',
             'project_updated',
+
             'company_client_status_changed',
             'company_client_created',
             'company_client_updated',
-            'soft_delete'
+
+            'soft_delete' -- um para cada tabela
         );
     END IF;
 END;
