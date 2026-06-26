5efd7ff36c0dbcf73589051aaf3c373ada30de21
msinis <eduarda.sinis@gmail.com>
2026-06-26 19:16:21 -0300

    fix: name for referenced table

--- database/migrations/V14__create_t_users.sql
+++ database/migrations/V14__create_t_users.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS registry.t_users(
-    id              UUID						NOT NULL,
-    display_name    VARCHAR(150)                NOT NULL,
-    is_enabled      registry.e_is_enabled_t_users  NOT NULL,
-    access_level    CHAR(1)                     NOT NULL,
-    created_at      TIMESTAMPTZ                 NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMPTZ                 NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    deleted_at      TIMESTAMPTZ                             DEFAULT NULL,
-    is_deleted      BOOLEAN                     NOT NULL    DEFAULT FALSE,
-    sector_id       INTEGER                     NOT NULL,
+    id              UUID						    NOT NULL,
+    display_name    VARCHAR(150)                    NOT NULL,
+    is_enabled      registry.e_is_enabled_t_users   NOT NULL,
+    access_level    CHAR(1)                         NOT NULL,
+    created_at      TIMESTAMPTZ                     NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMPTZ                     NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    deleted_at      TIMESTAMPTZ                         NULL    DEFAULT NULL,
+    is_deleted      BOOLEAN                         NOT NULL    DEFAULT FALSE,
+    sector_id       INTEGER                         NOT NULL,
 
     --PRIMARY KEY
     CONSTRAINT pk_profiles PRIMARY KEY(id),
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS registry.t_users(
 
     CONSTRAINT fk_profiles_sectors
         FOREIGN KEY(sector_id)
-        REFERENCES registry.sectors(id),
+        REFERENCES registry.t_sectors(id)
 
    
 );
