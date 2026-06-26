120e9d0d5fb6cd6dfae51e440108982a25ed5431
msinis <eduarda.sinis@gmail.com>
2026-06-26 19:17:12 -0300

    feat: add event types for logs

--- database/migrations/V17__create_e_type_t_logs.sql
+++ database/migrations/V17__create_e_type_t_logs.sql
@@ -4,40 +4,56 @@ BEGIN
         SELECT 1
         FROM pg_type t
         JOIN pg_namespace n ON n.oid = t.typnamespace
-        WHERE t.typname = 'logs.e_type_t_logs'
+        WHERE t.typname = 'e_event_type_t_logs'
           AND n.nspname = 'logs'
     ) THEN
         CREATE TYPE logs.e_event_type_t_logs
          AS ENUM (
-            'document_status_changed',
+            --Documents
+            'document_visibility_changed_to_public',
+            'document_visibility_changed_to_private',
             'document_created',
             'document_updated',
+            'document_deleted',
 
+            --Contracts
             'contract_created',
             'contract_expired',
             'contract_updated',
             'contract_type_created',
             'contract_type_updated',
 
-            'profile_created',
-            'profile_status_changed',
-            'profile_updated',
 
+            --Users
+            'user_created',
+            'user_is_enabled_changed_to_active',
+            'user_is_enabled_changed_to_on_leave',
+            'user_is_enabled_changed_to_terminated',
+            'user_updated',
+            'user_deleted'
+
+            --Sectors
             'sector_created',
             'sector_updated',
+            'sector_deleted',
 
+            --Categories
             'category_created',
             'category_updated',
+            'category_deleted',
 
-            'project_status_changed_created', -- colocar cada tipo de status
+            --Projects
+            'project_current_stage_changed_to_in_progress',
+            'project_current_stage_changed_to_finished' -- colocar cada tipo de status
             'project_created',
             'project_updated',
 
-            'company_client_status_changed',
+            --Companies_clientes
+            'company_client_is_enabled_changed_to_active',
+            'company_client_is_enabled_changed_to_terminated',
             'company_client_created',
-            'company_client_updated',
+            'company_client_updated'
 
-            'soft_delete' -- um para cada tabela
         );
     END IF;
 END;
