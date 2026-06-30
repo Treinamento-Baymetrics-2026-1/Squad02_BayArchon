773d171ef3da2c3cc84329f69a4dc1fcf0bdecda
msinis <eduarda.sinis@gmail.com>
2026-06-30 17:28:06 -0300

    fix: names for data

--- database/migrations/V14__create_t_users.sql
+++ database/migrations/V14__create_t_users.sql
@@ -1,13 +1,15 @@
 CREATE TABLE IF NOT EXISTS registry.t_users(
     id              UUID						    NOT NULL,
     display_name    VARCHAR(150)                    NOT NULL,
-    is_enabled      registry.e_is_enabled_t_users   NOT NULL,
+    situation       registry.e_situation_t_users    NOT NULL,
     access_level    CHAR(1)                         NOT NULL,
     created_at      TIMESTAMPTZ                     NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMPTZ                     NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     deleted_at      TIMESTAMPTZ                         NULL    DEFAULT NULL,
     is_deleted      BOOLEAN                         NOT NULL    DEFAULT FALSE,
+    is_active       BOOLEAN                         NOT NULL,
     sector_id       INTEGER                         NOT NULL,
+    created_by      UUID                            NOT NULL,
 
     --PRIMARY KEY
     CONSTRAINT pk_profiles PRIMARY KEY(id),
@@ -19,7 +21,7 @@ CREATE TABLE IF NOT EXISTS registry.t_users(
     CONSTRAINT chk_profiles_access_level
         CHECK(access_level IN('c','m','a')),
     
-     CONSTRAINT chk_users_soft_delete
+     CONSTRAINT chk_users_soft_deleted
         CHECK(
             functions.fn_is_not_deleted(
                 is_deleted,
@@ -28,13 +30,16 @@ CREATE TABLE IF NOT EXISTS registry.t_users(
         ),
 
     --FOREIGN KEY
-    CONSTRAINT fk_profiles_users
+    CONSTRAINT fk_users_id
         FOREIGN KEY(id)
         REFERENCES auth.users(id),
 
-    CONSTRAINT fk_profiles_sectors
+    CONSTRAINT fk_users_sectors
         FOREIGN KEY(sector_id)
-        REFERENCES registry.t_sectors(id)
+        REFERENCES registry.t_sectors(id),
 
-   
+    CONSTRAINT fk_users_created_by
+        FOREIGN KEY(created_by)
+        REFERENCES registry.t_users(id)
+
 );
