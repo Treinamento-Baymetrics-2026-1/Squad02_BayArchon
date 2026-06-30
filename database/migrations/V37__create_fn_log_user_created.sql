f48ff3fd49ad05c895bec58ac53e5ee909d0b2e6
msinis <eduarda.sinis@gmail.com>
2026-06-30 17:30:41 -0300

    cleanup: unnecessary data

--- database/migrations/V37__create_fn_log_user_created.sql
+++ database/migrations/V37__create_fn_log_user_created.sql
@@ -9,27 +9,22 @@ SET search_path = logs, pg_catalog
 AS $$
 BEGIN
     INSERT INTO logs.t_logs (
-        created_by,
-        created_for,
+        performed_by,
+        target_user,
         action_type,
         details
     )   VALUES (
-        NEW.id,
+        NEW.created_by,
         NEW.id,
         'user_created',
         json_build_object(
             'display_name', NEW.display_name,
-            'is_enabled', NEW.is_enabled,      
+            'situation', NEW.is_enabled,      
             'access_level', NEW.access_level,
-            'created_at', NEW.created_at, --Se é um log de criação, ainda é necessario?
-            'updated_at', NEW.updated_at, --Mesmo de cima
-            'deleted_at', NEW.deleted_at, --Se foi criado, vai ser NULL, é necessario?    
-            'is_deleted', NEW.is_deleted, --Mesmo de cima, aqui vai ser FALSE
+            'created_at', NEW.created_at, 
             'sector_id', NEW.sector_id       
         )
     );
     RETURN NEW;
 END;
 $$;
-
---Como fica os dados que vão para auth.users?
