6c3dbd531ec5bf04bb2ac7ce31828436e848e735
msinis <eduarda.sinis@gmail.com>
2026-07-03 13:47:37 -0300

    chore: fix name for log type

--- back-end/migrations/V44__create_fn_log_user_deleted.sql
+++ back-end/migrations/V44__create_fn_log_user_deleted.sql
@@ -17,7 +17,7 @@ BEGIN
         INSERT INTO logs.t_logs (
             performed_by,
             user_changed,
-            action_type,
+            type_logs,
             details
         )
         VALUES (
