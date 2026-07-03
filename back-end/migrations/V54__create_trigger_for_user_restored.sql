596e77446c69cef4adf63c4e2c9dafa57721953e
msinis <eduarda.sinis@gmail.com>
2026-07-03 13:48:56 -0300

    feat: add trigger for user_restored

---
+++ back-end/migrations/V54__create_trigger_for_user_restored.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER IF EXISTS registry_t_users_trg_log_user_restored
+ON registry.t_users;
+
+CREATE TRIGGER registry_t_users_trg_log_user_restored
+AFTER UPDATE ON registry.t_users
+FOR EACH ROW
+EXECUTE FUNCTION logs.fn_log_user_restored();
