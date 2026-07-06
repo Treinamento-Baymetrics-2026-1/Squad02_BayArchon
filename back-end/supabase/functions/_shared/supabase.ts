48fea2630cc44a60b7a1a770cb11bf568e547865
GuilhermeBayMetrics <guilherme.seredko@baymetrics.com.br>
2026-07-06 10:30:51 -0300

    Feat/UserAuth (#22)
    * feat: UserAuth functions created
    
    * fix: UserRegister DTO removed and UserFirstAccess user supabase client created
    
    ---------
    
    Co-authored-by: Guilherme Seredko <gseredko@baymetrics.com.br>

--- back-end/supabase/functions/_shared/supabase.ts
+++ back-end/supabase/functions/_shared/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase";
+import { Context } from "hono";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL") || "ERROR";
 
@@ -7,3 +8,16 @@ const supabaseRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") || "ERROR";
 
 
 export const supabase = createClient(supabaseUrl, supabaseRoleKey);
+
+export async function getSupabaseClient(context : Context){
+
+    const supabaseClient  = createClient(supabaseUrl, supabaseRoleKey, {
+        global: {
+            headers: {
+                Authorization: context.req.header("Authorization") || "",
+            },
+        },
+    });
+    
+    return supabaseClient;
+}
