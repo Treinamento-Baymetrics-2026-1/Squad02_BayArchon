8adc1f17fab7657c4e98db655b7ef8a5b5b6592e
msinis <eduarda.sinis@gmail.com>
2026-07-06 13:45:01 -0300

    feat: create function for sector created

---
+++ back-end/migrations/V57__create_fn_sector_created.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION logs.fn_log_sector_created()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+VOLATILE
+PARALLEL UNSAFE
+SECURITY INVOKER
+COST 1
+SET search_path = logs, pg_catalog
+AS $$
+BEGIN
+    INSERT INTO logs.t_logs (
+        performed_by,
+        type_logs,
+        details
+    )   VALUES (
+        auth.uid(),
+        'sector_created',
+        json_build_object(
+            'display_name', NEW.display_name,
+            'details', NEW.details,  
+            'is_deleted', NEW.is_deleted  
+        )
+    );
+    RETURN NEW;
+END;
+$$;
