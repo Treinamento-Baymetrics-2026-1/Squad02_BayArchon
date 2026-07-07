daaa2c00b19d7d0f6068a62c54c50361f848976a
msinis <eduarda.sinis@gmail.com>
2026-07-07 16:41:52 -0300

    chore: change name for trg

--- back-end/migrations/V069__create_trigger_for_document_visibility_changed_to_public.sql
+++ back-end/migrations/V069__create_trigger_for_document_visibility_changed_to_public.sql
@@ -1,7 +1,7 @@
-DROP TRIGGER IF EXISTS documents_t_documents_trg_log_document_visibility_changed_to_public
+DROP TRIGGER IF EXISTS documents_t_documents_trg_log_document_visibility_changed_to_pu
 ON documents.t_documents;
 
-CREATE TRIGGER documents_t_documents_trg_log_document_visibility_changed_to_public
+CREATE TRIGGER documents_t_documents_trg_log_document_visibility_changed_to_pu
 AFTER UPDATE ON documents.t_documents
 FOR EACH ROW
 WHEN(
