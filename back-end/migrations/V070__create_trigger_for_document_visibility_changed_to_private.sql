34002eebeeea1e5f4343d63c9f3147ec33ef2b82
msinis <eduarda.sinis@gmail.com>
2026-07-07 16:42:02 -0300

    chore: change name for trg

--- back-end/migrations/V070__create_trigger_for_document_visibility_changed_to_private.sql
+++ back-end/migrations/V070__create_trigger_for_document_visibility_changed_to_private.sql
@@ -1,7 +1,7 @@
-DROP TRIGGER IF EXISTS documents_t_documents_trg_log_document_visibility_changed_to_private
+DROP TRIGGER IF EXISTS documents_t_documents_trg_log_document_visibility_changed_to_pr
 ON documents.t_documents;
 
-CREATE TRIGGER documents_t_documents_trg_log_document_visibility_changed_to_private
+CREATE TRIGGER documents_t_documents_trg_log_document_visibility_changed_to_pr
 AFTER UPDATE ON documents.t_documents
 FOR EACH ROW
 WHEN(
