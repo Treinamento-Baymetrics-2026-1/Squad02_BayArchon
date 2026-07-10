162c904575a8de4d986f842e6d65027b30615ea0
GuilhermeBayMetrics <guilherme.seredko@baymetrics.com.br>
2026-07-10 10:32:42 -0300

    Feat/UserAuth (#26)
    * feat: UserAuth functions created
    
    * fix: UserRegister DTO removed and UserFirstAccess user supabase client created
    
    * feat: SendRedefinePassword and Login functions created
    
    ---------
    
    Co-authored-by: Guilherme Seredko <gseredko@baymetrics.com.br>

--- back-end/supabase/functions/Auth/AuthController.ts
+++ back-end/supabase/functions/Auth/AuthController.ts
@@ -1,8 +1,10 @@
 import { Context } from "hono";
 import {RegisterService} from "./AuthServices/RegisterService.ts";
-import {FirstAccessService} from "./AuthServices/FirstAccessService.ts";
+import {UpdatePasswordService} from "./AuthServices/UpdatePasswordService.ts";
 import { SetProfileActive } from "../Profiles/ProfilesServices/SetProfileActive.ts";
-import { AuthRegisterSchema, AuthFirstAccessSchema } from "./AuthSchema.ts"; 
+import { LoginService } from "./AuthServices/LoginService.ts";
+import { SendRedefinePasswordService } from "./AuthServices/SendRedefinePasswordService.ts";
+import { AuthRegisterSchema, AuthFirstAccessSchema, AuthLoginSchema, AuthRedefinePasswordSchema } from "./AuthSchema.ts"; 
 import { supabase, getSupabaseClient } from "../_shared/supabase.ts";
 
 export async function Register(context: Context){
@@ -53,8 +55,87 @@ export async function FirstAccess(context: Context){
 
     try{
 
-        const result = await FirstAccessService(supabaseUser, parsedFirstAccess.data);
+        const result = await UpdatePasswordService(supabaseUser, parsedFirstAccess.data);
         await SetProfileActive(supabaseUser);
+        return context.newResponse(JSON.stringify({ body: result }), 200,{"Content-Type": "application/json"});
+
+    }catch(err){
+
+        return context.newResponse(JSON.stringify({error: (err as Error).message}),400,{"Content-Type": "application/json"});
+        
+    }
+}
+
+export async function Login(context: Context){
+
+    const body = await context.req.json();
+
+    const parsedBody = AuthLoginSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+            return context.json(
+                { error: parsedBody.error },
+                400
+            );
+        }
+
+    try{
+        const result = await LoginService(supabase, parsedBody.data);
+        return context.json({body: result})
+    }catch(err){
+        return context.newResponse(JSON.stringify({error: (err as Error).message}),500,{"Content-Type": "application/json"});
+    }
+}
+
+export async function SendRedefinePassword(context : Context){
+    
+    const body = await context.req.json();
+
+    const parsedBody = AuthRedefinePasswordSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+            return context.json(
+                { error: parsedBody.error },
+                400
+            );
+        }
+
+    try{
+        const result = await SendRedefinePasswordService(supabase, parsedBody.data);
+        return context.json(result);
+    }
+    catch(err){
+        return context.newResponse(JSON.stringify({error: (err as Error).message}),500,{"Content-Type": "application/json"});
+    }
+}
+
+export async function UpdatePassword(context: Context){
+    const body = await context.req.json();
+
+    const parsedFirstAccess = AuthFirstAccessSchema.safeParse(body);
+
+    if (!parsedFirstAccess.success) {
+        return new Response(
+            JSON.stringify({ error: parsedFirstAccess.error }),
+            { status: 400 }
+        );
+    }
+
+    const authorization = context.req.header("Authorization");
+
+    if (!authorization) {
+    return context.json(
+        { error: "Authorization header is required." },
+        401
+    );
+}
+
+    const supabaseUser = await getSupabaseClient(context);
+
+    try{
+
+        const result = await UpdatePasswordService(supabaseUser, parsedFirstAccess.data);
+
         return context.newResponse(JSON.stringify({ body: result }), 200,{"Content-Type": "application/json"});
 
     }catch(err){
