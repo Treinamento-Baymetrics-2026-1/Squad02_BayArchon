5347bd84e8fbfbcb340a7ecc43c9e2d8775516ea
msinis <eduarda.sinis@gmail.com>
2026-07-10 17:04:13 -0300

    fix: data with wrong name

--- back-end/migrations/V098__create_fn_log_deleted_t_contract_types.sql
+++ back-end/migrations/V098__create_fn_log_deleted_t_contract_types.sql
@@ -14,7 +14,7 @@ BEGIN
         details
     )   VALUES(
         auth.uid(),
-        'contract_type_updated',
+        'contract_type_deleted',
         json_build_object(
             'id', NEW.id,
             'display_name', NEW.display_name,
