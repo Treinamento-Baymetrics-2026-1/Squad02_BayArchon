82787364a40392a30d428a843ce5d1d76e1b00f1
msinis <eduarda.sinis@gmail.com>
2026-07-15 15:24:38 -0300

    fix: back to origin version is_enabled

--- back-end/migrations/V007__create_t_companies_clients.sql
+++ back-end/migrations/V007__create_t_companies_clients.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS registry.t_companies_clients(
     id              UUID            NOT NULL    DEFAULT gen_random_uuid() ,
     display_name    VARCHAR(200)    NOT NULL,
     details         VARCHAR(3000)   NOT NULL,
-    is_active       BOOLEAN         NOT NULL,
+    is_enabled      CHAR(1)         NOT NULL    DEFAULT 'a',
     cnpj            VARCHAR(14)         NULL,
     email           VARCHAR(320)    NOT NULL,
     created_at      TIMESTAMPTZ     NOT NULL    DEFAULT CURRENT_TIMESTAMP,
@@ -21,6 +21,9 @@ CREATE TABLE IF NOT EXISTS registry.t_companies_clients(
     
     CONSTRAINT chk_companies_clients_details
         CHECK(functions.fn_is_valid_text(details)),
+
+    CONSTRAINT chk_companies_clients_status
+        CHECK(is_enabled IN('a','t')),
 
     CONSTRAINT chk_companies_clients_cnpj
         CHECK(functions.fn_is_valid_cnpj(cnpj)),
