80e795251c2b21914d548a92c4af93d164b78513
msinis <eduarda.sinis@gmail.com>
2026-07-16 16:34:26 -0300

    style: columns

--- back-end/migrations/V018__create_t_logs.sql
+++ back-end/migrations/V018__create_t_logs.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS logs.t_logs(
     ),
 
     type_logs       logs.e_event_type_t_logs     NOT NULL,
-    performed_by    UUID                             NULL,
-    user_changed    UUID                             NULL, 
     details         JSONB                        NOT NULL,
     created_at      TIMESTAMPTZ                  NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    performed_by    UUID                             NULL,
+    user_changed    UUID                             NULL, 
 
     --PRIMARY KEY
     CONSTRAINT pk_logs PRIMARY KEY(id),
